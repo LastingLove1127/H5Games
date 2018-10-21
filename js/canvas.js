364bc97fe077b17ed1aa4f68c4f7feabd76fc249
LastingLove1127 <584033744@qq.com>
2018-10-21 19:55:06 +0800

    save lastest version

--- js/canvas.js
+++ js/canvas.js
@@ -10,22 +10,49 @@ $(function(){
 	ctx.translate(r+50,r+50);
 	for(var i=0;i<12;i++){
 		var deg=i*Math.PI/6;
+		var rad=deg/180;
 		var x=r*Math.cos(deg);
 		var y=r*Math.sin(deg);
-		var txtDeg=Math.ceil(deg*180/Math.PI)+"°";
+		var txtDeg="角度："+Math.ceil(deg*180/Math.PI)+"°";
+		var txtRad="弧度:"+Math.ceil(rad);
 		toLine(ctx,x,y,r);
 		if(x>0 && y<0){
-			txtFill(ctx,txtDeg,x,y-10);
+			txtFill(ctx,txtDeg,x,y-5);
 		}else if(x<0 && y>0){
-			txtFill(ctx,txtDeg,x-30,y+10);			
+			txtFill(ctx,txtDeg,x-5,y+5);		
 		}else if(x<0 && y<0){
-			txtFill(ctx,txtDeg,x-30,y-10);
+			txtFill(ctx,txtDeg,x-5,y-5);
 		}else if(x>0 && y>0){
-			txtFill(ctx,txtDeg,x,y+20);			
+			txtFill(ctx,txtDeg,x-5,y+5);		
 		}else{
 			txtFill(ctx,txtDeg,x,y);		
 		}
 	}
+	ctx.translate(-r-50,-r-50);
+	//绘制上半圆
+	ctx.beginPath();
+	ctx.fillStyle="yellow";	
+	ctx.arc(100,500,100,0,Math.PI,true);
+	ctx.closePath();
+	ctx.fill();
+	//绘制下半圆
+	ctx.beginPath();	
+	ctx.fillStyle="orange";	
+	ctx.arc(100,520,100,0,Math.PI,false);
+	ctx.closePath();
+	ctx.fill();
+	//绘制左半圆
+	ctx.beginPath();	
+	ctx.fillStyle="green";	
+	ctx.arc(310,520,100,Math.PI/2,Math.PI*3/2,false);
+	ctx.closePath();
+	ctx.fill();
+	//绘制右半圆
+	ctx.beginPath();
+	ctx.fillStyle="#9cb";	
+	ctx.arc(330,520,100,Math.PI*3/2,Math.PI/2,false);
+	ctx.closePath();
+	ctx.fill();
 })
 
 function drawCircle(ctx,x,y){
@@ -45,6 +72,6 @@ function toLine(ctx,x,y,r){
 }
 
 function txtFill(ctx,txt,x,y){
-	ctx.font="16px Arial";
+	ctx.font="14px Arial";
 	ctx.fillText(txt,x,y);
 }
--- js/untangle.js
+++ js/untangle.js
@@ -1,14 +1,79 @@
+function Circle(x,y,r){
+	this.x=x;
+	this.y=y;
+	this.r=r;
+}
+function Line(startPoint,endPoint,thickness){
+	this.startPoint=startPoint;
+	this.endPoint=endPoint;
+	this.thickness;
+}
+var untangleGame={
+	circle:[],
+	thinLineThickness:1,
+	lines:[]
+}
+function drawLine(ctx,x1,y1,x2,y2,thickness){
+	ctx.beginPath();
+	ctx.moveTo(x1,y1);
+	ctx.lineTo(x2,y2);
+	ctx.lineWidth=thickness;
+	ctx.strokeStyle="#c9c";
+	ctx.closePath()
+	ctx.stroke();
+}
+function drawCircle(ctx,x,y,radius){
+	var rgbR=Math.random()*155+100;
+	var rgbG=Math.random()*155+120;
+	var rgbB=Math.random()*155+100;
+	var rgbA=Math.random();
+	ctx.beginPath();
+	ctx.fillStyle="rgba("+rgbR+","+rgbG+","+rgbB+","+rgbA+")";
+	ctx.arc(x,y,radius,0,Math.PI*2,false);
+	ctx.closePath();
+	ctx.fill();
+}
+
 $(function(){
 	/**
 	 * 不能使用$("#game")这是jQuery对象
 	 * 只能使用document.getElementById();
 	 */
 	var canvas=document.getElementById("game");
-	console.log(canvas);
+	console.log(canvas.width,canvas.height);
 	var ctx=canvas.getContext("2d");
-	ctx.fillStyle="rgba(200,200,100,.6)";
-	ctx.beginPath();
-	ctx.arc(100,100,50,0,Math.PI*2,true);
-	ctx.closePath();
-	ctx.fill();
+	var width=canvas.width;
+	var height=canvas.height;
+	
+	for(var i=0;i<5;i++){
+		var x=Math.random()*width;
+		var y=Math.random()*height;		
+		var radius=(Math.random()*20)+10;
+		txtFill(ctx,i,x,y);
+		drawCircle(ctx,x,y,radius);
+		untangleGame.circle.push(new Circle(x,y,radius));
+	}
+
+	var circle=untangleGame.circle;
+	var thickness=untangleGame.thinLineThickness;
+	var len=circle.length;
+	for(var i=0;i<len;i++){
+		startPoint=circle[i];
+		for(var j=0;j<i;j++){
+			endPoint=circle[j];
+			drawLine(ctx,startPoint.x,startPoint.y,endPoint.x,endPoint.y,thickness);
+		untangleGame.lines.push(new Line(startPoint,endPoint,thickness));
+		}
+
+	}
+
 })
+
+function txtFill(ctx,txt,x,y){
+	ctx.font="30px Arial";
+	ctx.fillText(txt,x,y);
+}
+
+function clearctx(ctx){
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+}
